4dc21cdbf455f16b95777ff43f6ef89cd8e7d3ca
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-28 16:09:28 +0900

    add copyright

--- cmake/grpc.cmake
+++ cmake/grpc.cmake
@@ -1,3 +1,24 @@
+# Forked from https://github.com/grpc/grpc/blob/master/examples/cpp/cmake/common.cmake
+
+# Copyright 2018 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# cmake build file for C++ route_guide example.
+# Assumes protobuf and gRPC have been installed using cmake.
+# See cmake_externalproject/CMakeLists.txt for all-in-one cmake build
+# that automatically builds all the dependencies before building route_guide.
+
 include(FetchContent)
 
 set(FETCHCONTENT_BASE_DIR ${EXTERNALDIR}/)
