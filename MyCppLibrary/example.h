153c10a5fc8c036cbf2aca8888c2b5ec6b5c095e
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-28 13:24:40 +0900

    Add cache of CI (#13)
    * add cache of ci
    
    * fix
    
    * fix
    
    * tidy
    
    * use hash
    
    * update googletest version
    
    * tidy

--- MyCppLibrary/example.h
+++ MyCppLibrary/example.h
@@ -1,9 +1,8 @@
 #ifndef MYCPPLIBRARY_EXAMPLE_H
 #define MYCPPLIBRARY_EXAMPLE_H
 
-namespace MyCppLibrary
-{
-    int f();
+namespace MyCppLibrary {
+int f();
 }
 
-#endif //MYCPPLIBRARY_EXAMPLE_H
+#endif // MYCPPLIBRARY_EXAMPLE_H
--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -4,6 +4,6 @@ set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        release-1.8.0
+  GIT_TAG        release-1.10.0
 )
 FetchContent_MakeAvailable(googletest)
