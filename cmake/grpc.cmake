cf8bfd0bfec8ba47f3e92ff05097bd98199ec6c2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-28 13:35:37 +0900

    test grpc

---
+++ cmake/grpc.cmake
@@ -0,0 +1,19 @@
+include(FetchContent)
+
+set(FETCHCONTENT_BASE_DIR ${EXTERNALDIR}/)
+set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
+
+FetchContent_Declare(
+  grpc
+  GIT_REPOSITORY https://github.com/grpc/grpc.git
+  GIT_TAG        v1.36.4
+)
+
+FetchContent_MakeAvailable(grpc)
+
+set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+set(_REFLECTION grpc++_reflection)
+set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+set(_GRPC_GRPCPP grpc++)
+set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+
